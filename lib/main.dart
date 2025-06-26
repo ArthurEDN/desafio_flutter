daf0044bebf6290ddcab32dc9ce9159f8c2af982
ArthurEDN <arthuredn02@gmail.com>
2025-06-25 23:53:48 -0300

    Instanciando o Locator para injeção de dependências. Criação do MultiBlocProvider para propagar os blocs para as pages. Criação do routerConfig no MaterialApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,41 @@
+import 'package:desafio_flutter/core/config/dependecies.dart';
+import 'package:desafio_flutter/core/themes/theme.dart';
+import 'package:desafio_flutter/presentation/auth/bloc/auth_bloc.dart';
+import 'package:desafio_flutter/presentation/auth/login/bloc/login_bloc.dart';
+import 'package:desafio_flutter/presentation/auth/register/bloc/register_bloc.dart';
+import 'package:desafio_flutter/presentation/home/bloc/home_bloc.dart';
+import 'package:desafio_flutter/routing/router.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  setupLocator();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(create: (context) => locator<AuthBloc>()),
+        BlocProvider<LoginBloc>(create: (context) => locator<LoginBloc>()),
+        BlocProvider<RegisterBloc>(
+          create: (context) => locator<RegisterBloc>(),
         ),
+        BlocProvider<HomeBloc>(create: (context) => locator<HomeBloc>()),
+      ],
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: AppTheme.darkTheme,
+        routerConfig: router,
+        debugShowCheckedModeBanner: false,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
